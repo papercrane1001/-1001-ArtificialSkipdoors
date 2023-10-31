3f0ec32085a2cf7649bcbfc42a96b0a341b3a5a1
papercrane1001 <papercrane1001@gmail.com>
2023-10-31 10:29:31 -0700

    why not transpilers

--- [1001]ArtificialSkipdoors/HarmonyPatches.cs
+++ [1001]ArtificialSkipdoors/HarmonyPatches.cs
@@ -16,36 +16,39 @@ namespace _1001_ArtificialSkipdoors
     [StaticConstructorOnStartup]
     static class HarmonyPatches
     {
+
         static HarmonyPatches()
         {
-            var harmony = new Harmony("rimworld.OneThousandOne.ArtificialSkipdoors");
+            //var harmony = new Harmony("rimworld.OneThousandOne.ArtificialSkipdoors");
 
-            System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Skipdoor), "SpawnSetup");
-            var mPrefix = AccessTools.Method(typeof(HarmonyPatches), "SpawnSetup_Postfix");
+            //System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Skipdoor), "SpawnSetup");
+            //var mPrefix = AccessTools.Method(typeof(HarmonyPatches), "SpawnSetup_Postfix");
 
-            harmony.Patch(mOriginal, null, new HarmonyMethod(mPrefix));
+            //harmony.Patch(mOriginal, null, new HarmonyMethod(mPrefix));
+
+            
         }
-        //public static bool SpawnSetup_Postfix(ref Map map, ref bool respawningAfterLoad)
-        public static bool SpawnSetup_Prefix(ref bool __result, ref Skipdoor __instance, Map map, bool respawningAfterLoad)//Wrong order?  
-        {
-            //System.Reflection.MethodInfo mCalculateGrowth = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "CalculateGrowth");
-            //mCalculateGrowth.Invoke(__instance, new object[] { 60 * +60000 });
+        ////public static bool SpawnSetup_Postfix(ref Map map, ref bool respawningAfterLoad)
+        //public static bool SpawnSetup_Prefix(ref bool __result, ref Skipdoor __instance, Map map, bool respawningAfterLoad)//Wrong order?  
+        //{
+        //    //System.Reflection.MethodInfo mCalculateGrowth = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "CalculateGrowth");
+        //    //mCalculateGrowth.Invoke(__instance, new object[] { 60 * +60000 });
 
-            System.Reflection.MethodInfo mSpawnSetup = AccessTools.Method(typeof(Skipdoor), "SpawnSetup");
-            mSpawnSetup.Invoke(__instance, new object[] { map, respawningAfterLoad });
+        //    System.Reflection.MethodInfo mSpawnSetup = AccessTools.Method(typeof(Skipdoor), "SpawnSetup");
+        //    mSpawnSetup.Invoke(__instance, new object[] { map, respawningAfterLoad });
 
-            NVPESkipDoor check = __instance as NVPESkipDoor;
-            if (check == null)
-            {
-                Log.Message("HarPing1");
-                return true;
-            }
-            else
-            {
-                //__instance.SpawnSetup()
-                Log.Message("HarPing2");
-                return false;
-            }
-        }
+        //    NVPESkipDoor check = __instance as NVPESkipDoor;
+        //    if (check == null)
+        //    {
+        //        Log.Message("HarPing1");
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        //__instance.SpawnSetup()
+        //        Log.Message("HarPing2");
+        //        return false;
+        //    }
+        //}
     }
 }
