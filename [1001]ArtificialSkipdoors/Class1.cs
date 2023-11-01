151510d91adda2d9aa281f4099c461f36d013324
papercrane1001 <papercrane1001@gmail.com>
2023-11-01 09:42:52 -0700

    wew

--- [1001]ArtificialSkipdoors/Class1.cs
+++ [1001]ArtificialSkipdoors/Class1.cs
@@ -24,6 +24,7 @@ namespace _1001_ArtificialSkipdoors
     {
         //public NVPESkipDoor door;
         //public Pawn pawn;
+        public Skipdoor pdoor;
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
             //pawn = new Pawn();
@@ -35,11 +36,11 @@ namespace _1001_ArtificialSkipdoors
             //GenSpawn.Spawn(door, Position + new IntVec3(1, 0, 0), map, WipeMode.Vanish);
             Log.Message("Ping1");
 
-            Skipdoor Sdoor = (Skipdoor)ThingMaker.MakeThing(VPE_DefOf.VPE_Skipdoor, null);
-            if(Sdoor == null) { Log.Message("Sdoor null"); }
+            pdoor = (Skipdoor)ThingMaker.MakeThing(VPE_DefOf.VPE_Skipdoor, null);
+            if(pdoor == null) { Log.Message("Sdoor null"); }
             Log.Message("Ping2");
-            Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(Sdoor));
-            GenSpawn.Spawn(Sdoor, Position + new IntVec3(-1, 0, 0), map, WipeMode.Vanish);
+            Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(pdoor));
+            GenSpawn.Spawn(pdoor, Position + new IntVec3(-1, 0, 0), map, WipeMode.Vanish);
             Log.Message("Ping3");
         }
 
@@ -61,170 +62,4 @@ namespace _1001_ArtificialSkipdoors
             //}
         }
     }
-    //public class NVPEDoorPlaceWorker : PlaceWorker
-    //{
-    //    public override void PostPlace(Map map, BuildableDef def, IntVec3 loc, Rot4 rot)
-    //    {
-    //        base.PostPlace(map, def, loc, rot);
-    //        //Skipdoor door = new Skipdoor()
-    //        //Skipdoor door = WorldComponent_DoorTeleporterManager.Instance.DoorTeleporters.
-    //        //Skipdoor door = (Skipdoor)ThingMaker.MakeThing(VPE_DefOf.VPE_Skipdoor, null);
-    //        //Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(door));
-    //        //GenSpawn.Spawn(door, loc, map, WipeMode.Vanish);
-
-
-    //        //NVPEDoorTeleporter door = new NVPEDoorTeleporter();
-    //        //WorldComponent_DoorTeleporterManager.Instance.DoorTeleporters.Add(door);
-    //    }
-    //}
-
-    //public class NVPESkipDoor : Skipdoor
-    //{
-    //    //public override 
-    //    public override void SpawnSetup(Map map, bool respawningAfterLoad)
-    //    {
-    //        base.SpawnSetup(map, respawningAfterLoad);
-    //        //#region Thing
-    //        //map.listerThings.Add(this);
-    //        //map.thingGrid.Register(this);
-    //        //map.gasGrid.Notify_ThingSpawned(this);
-    //        //map.mapTemperature.Notify_ThingSpawned(this);
-    //        //if (map.IsPlayerHome)
-    //        //{
-    //        //    EverSeenByPlayer = true;
-    //        //}
-
-    //        //if (Find.TickManager != null)
-    //        //{
-    //        //    Find.TickManager.RegisterAllTickabilityFor(this);
-    //        //}
-
-    //        //DirtyMapMesh(map);
-    //        //if (def.drawerType != DrawerType.MapMeshOnly)
-    //        //{
-    //        //    map.dynamicDrawManager.RegisterDrawable(this);
-    //        //}
-
-    //        //map.tooltipGiverList.Notify_ThingSpawned(this);
-    //        //if (def.CanAffectLinker)
-    //        //{
-    //        //    map.linkGrid.Notify_LinkerCreatedOrDestroyed(this);
-    //        //    map.mapDrawer.MapMeshDirty(Position, MapMeshFlag.Things, regenAdjacentCells: true, regenAdjacentSections: false);
-    //        //}
-
-    //        //if (def.pathCost != 0 || def.passability == Traversability.Impassable)
-    //        //{
-    //        //    map.pathing.RecalculatePerceivedPathCostUnderThing(this);
-    //        //}
-
-    //        //if (def.AffectsReachability)
-    //        //{
-    //        //    map.reachability.ClearCache();
-    //        //}
-
-    //        //map.coverGrid.Register(this);
-    //        //if (def.category == ThingCategory.Item)
-    //        //{
-    //        //    map.listerHaulables.Notify_Spawned(this);
-    //        //    map.listerMergeables.Notify_Spawned(this);
-    //        //}
-
-    //        //map.attackTargetsCache.Notify_ThingSpawned(this);
-    //        //(map.regionGrid.GetValidRegionAt_NoRebuild(Position)?.Room)?.Notify_ContainedThingSpawnedOrDespawned(this);
-    //        //StealAIDebugDrawer.Notify_ThingChanged(this);
-    //        //IHaulDestination haulDestination = this as IHaulDestination;
-    //        //if (haulDestination != null)
-    //        //{
-    //        //    map.haulDestinationManager.AddHaulDestination(haulDestination);
-    //        //}
-
-    //        //if (this is IThingHolder && Find.ColonistBar != null)
-    //        //{
-    //        //    Find.ColonistBar.MarkColonistsDirty();
-    //        //}
-
-    //        //if (def.category == ThingCategory.Item)
-    //        //{
-    //        //    SlotGroup slotGroup = Position.GetSlotGroup(map);
-    //        //    if (slotGroup != null && slotGroup.parent != null)
-    //        //    {
-    //        //        slotGroup.parent.Notify_ReceivedThing(this);
-    //        //    }
-    //        //}
-
-    //        //if (def.receivesSignals)
-    //        //{
-    //        //    Find.SignalManager.RegisterReceiver(this);
-    //        //}
-
-    //        //if (!respawningAfterLoad)
-    //        //{
-    //        //    QuestUtility.SendQuestTargetSignals(questTags, "Spawned", this.Named("SUBJECT"));
-    //        //}
-    //        //#endregion
-
-    //        //#region ThingWithComps
-    //        //Log.Message("Ping1");
-    //        ////((ThingWithComps)this).SpawnSetup(map, respawningAfterLoad);
-    //        ////ThingWithComps.prototype.SpawnSetup(map, respawningAfterLoad);
-    //        ////var ptr = typeof(ThingWithComps).GetMethod("SpawnSetup").MethodHandle.GetFunctionPointer();
-    //        //////Func<>
-    //        ////var ThingWithCompsSpawnSetup = (Action)Activator.CreateInstance(typeof(Action), this, ptr);
-    //        ////ThingWithCompsSpawnSetup();
-
-
-
-
-
-    //        //Log.Message("Ping2");
-
-    //        ////if (AllComps != null)
-    //        ////{
-    //        ////    for(int i = 0; i < AllComps.Count; i++)
-    //        ////    {
-    //        ////        AllComps[i].PostSpawnSetup(respawningAfterLoad);
-    //        ////    }
-    //        ////}
-    //        ////if (comps != null)
-    //        ////{
-    //        ////    for (int i = 0; i < comps.Count; i++)
-    //        ////    {
-    //        ////        comps[i].PostSpawnSetup(respawningAfterLoad);
-    //        ////    }
-    //        ////}
-    //        //#endregion
-
-    //        //#region DoorTeleporter
-    //        //WorldComponent_DoorTeleporterManager.Instance.DoorTeleporters.Add((Skipdoor)this);
-    //        //DoorTeleporterMaterials mat = doorTeleporterMaterials[def];
-    //        //LongEventHandler.ExecuteWhenFinished(delegate
-    //        //{
-    //        //    background1 = new RenderTexture(mat.backgroundTex.width, mat.backgroundTex.height, 0);
-    //        //    background2 = new RenderTexture(mat.backgroundTex.width, mat.backgroundTex.height, 0);
-    //        //    backgroundMat = new Material(ShaderDatabase.TransparentPostLight);
-    //        //    RecacheBackground();
-    //        //});
-    //        //#endregion
-
-    //        //#region Skipdoor
-
-    //        //#endregion
-    //    }
-    //}
-
-    
-    //public class NVPEAbility_Skipdoor : VFECore.Abilities.Ability
-    //{
-    //    public override void Cast(params GlobalTargetInfo[] targets)
-    //    {
-    //        base.Cast(targets);
-    //        foreach(GlobalTargetInfo target in targets)
-    //        {
-    //            Skipdoor skipdoor = (Skipdoor)ThingMaker.MakeThing(VPE_DefOf.VPE_Skipdoor, null);
-    //            //skipdoor.Pawn = this.pawn;
-    //            Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(skipdoor));
-    //            GenSpawn.Spawn(skipdoor, target.Cell, target.Map, WipeMode.Vanish);
-    //        }
-    //    }
-    //}
 }
--- [1001]ArtificialSkipdoors/HarmonyPatches.cs
+++ [1001]ArtificialSkipdoors/HarmonyPatches.cs
@@ -27,22 +27,75 @@ namespace _1001_ArtificialSkipdoors
         {
             Log.Message("ASTranspiler Running");
             List<CodeInstruction> instructionList = instructions.ToList();
+            bool found = false;
             
             for (int i = 0; i < instructionList.Count; i++)
             {
-                if (instructionList[i].opcode == OpCodes.Ldarg_0 && i + 2 < instructionList.Count)
+                if (instructionList[i].opcode == OpCodes.Ldarg_0 && i + 2 < instructionList.Count && found == false)
                 {
+                    Log.Message("Found 'this'");
                     // Look for the "ldarg.0" instruction, which loads the "this" argument
                     // Check if the subsequent instructions match the pattern for accessing "this.Pawn"
-                    if (instructionList[i + 1].opcode == OpCodes.Ldfld && instructionList[i + 2].operand is FieldInfo fieldInfo && fieldInfo.Name == "Pawn")
+                    if(instructionList[i + 1].operand is FieldInfo fieldInfo_pawn && fieldInfo_pawn.Name == "Pawn")
                     {
-                        Log.Message("if's passed");
-                        // Insert your null check and return logic
-                        yield return new CodeInstruction(OpCodes.Ldarg_0);
-                        yield return new CodeInstruction(OpCodes.Ldfld, fieldInfo);
-                        yield return new CodeInstruction(OpCodes.Brfalse, instructionList[i]);
-                        yield return instructionList[i];
+                        Log.Message("Found this.Pawn");
+                        //Log.Message("fieldInfo.Name: " + ((FieldInfo)instructionList[i + 2].operand).Name); // <-- threw an error
+                        //if (instructionList[i+2].operand is FieldInfo fieldInfo_Psycasts)
+                        //{
+                        //    Log.Message("fieldInfo.Name: " + fieldInfo_pawn.Name);
+                        //}
+                        //if(fieldInfo.Name == "")
+                        Log.Message("Point: " + instructionList[i + 2].operand.ToString()); // Should be VanillaPsycastsExpanded.Hediff_PsycastAbilities Psycasts(Verse.Pawn)
+
+                        if (instructionList[i + 2].opcode == OpCodes.Call)
+                        {
+                            Log.Message("Found method call, running patch");
+                            found = true;
+                            //Log.Message(fieldInfo_Psycasts.Name);
+                            yield return new CodeInstruction(OpCodes.Ldarg_0);
+                            yield return new CodeInstruction(OpCodes.Ldfld, fieldInfo_pawn);
+                            yield return new CodeInstruction(OpCodes.Brfalse, instructionList[i]);
+                            yield return instructionList[i];
+                        }
+                        else
+                        {
+                            yield return instructionList[i];
+                        }
+
+                        //if (instructionList[i + 2].operand is FieldInfo fieldInfo_Psycasts)// && fieldInfo_Psycasts.Name != "psychicEntropy")
+                        //{
+                        //    Log.Message("Made fieldInfo_Psycasts");
+                        //    if (instructionList[i+2].opcode == OpCodes.Call)
+                        //    {
+                        //        Log.Message("Found method call, running patch");
+                        //        found = true;
+                        //        //Log.Message(fieldInfo_Psycasts.Name);
+                        //        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        //        yield return new CodeInstruction(OpCodes.Ldfld, fieldInfo_pawn);
+                        //        yield return new CodeInstruction(OpCodes.Brfalse, instructionList[i]);
+                        //        yield return instructionList[i];
+                        //    }
+                        //    else
+                        //    {
+                        //        yield return instructionList[i];
+                        //    }
+                            
+                        //}
+                        //else
+                        //{
+                        //    yield return instructionList[i];
+                        //}
                     }
+                    //instructionList[i + 2].operand is FieldInfo fieldInfo;
+                    //if (instructionList[i + 1].opcode == OpCodes.Ldfld && instructionList[i + 2].operand is FieldInfo fieldInfo && fieldInfo.Name == "Pawn")
+                    //{
+                    //    Log.Message("if's passed");
+                    //    // Insert your null check and return logic
+                    //    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    //    yield return new CodeInstruction(OpCodes.Ldfld, fieldInfo);
+                    //    yield return new CodeInstruction(OpCodes.Brfalse, instructionList[i]);
+                    //    yield return instructionList[i];
+                    //}
                     else
                     {
                         yield return instructionList[i];
@@ -56,6 +109,16 @@ namespace _1001_ArtificialSkipdoors
         }
     }
 
-    
-    
+    /*
+    public override void SpawnSetup(Map map, bool respawningAfterLoad)
+    {
+        base.SpawnSetup(map, respawningAfterLoad);
+        this.Pawn.Psycasts().AddMinHeatGiver(this);
+        if (!respawningAfterLoad)
+        {
+            this.Pawn.psychicEntropy.TryAddEntropy(50f, this, true, true);
+        }
+    }
+    */
+
 }
