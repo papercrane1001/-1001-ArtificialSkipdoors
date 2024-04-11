6932affa23a1b963a61ef9123b127f8ce6cdaa72
papercrane1001 <papercrane1001@gmail.com>
2024-04-11 14:29:43 -0700

    Who knows

---
+++ 1.5/[1001]ArtificialSkipdoors/ModEntry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime;
+using HarmonyLib;
+using JetBrains.Annotations;
+//using TerrainMovement;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace _1001_ArtificialSkipdoors
+{
+    [UsedImplicitly]
+    public class ModEntry : Mod
+    {
+        public ModEntry(ModContentPack content) : base(content)
+        {
+            var har = new Harmony(Content.PackageIdPlayerFacing);
+            System.Reflection.MethodInfo mOriginal = 
+                AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "SpawnSetup");
+            var mTranspiler = AccessTools.Method(
+                typeof(SkipDoor_SpawnSetup_Transpiler),
+                "SkipDoor_SpawnSetup_Actual_Transpiler");
+            if(har == null) { Log.Message("har null"); }
+            if(mOriginal == null) { Log.Message("mOriginal null"); }
+            if(mTranspiler == null) { Log.Message("mTranspiler null"); }
+            har.Patch(mOriginal, null, null, new HarmonyMethod(mTranspiler));
+
+            System.Reflection.MethodInfo mOGUI = 
+                AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "GetDoorTeleporterGismoz");
+            var mTrGUI = AccessTools.Method(
+                typeof(SkipDoor_GetDoorTeleporterGismoz_Transpiler),
+                "SkipDoor_GetDoorTeleporterGismoz_Actual_Transpiler");
+            har.Patch(mOGUI, null, null, new HarmonyMethod(mTrGUI));
+        }
+
+        
+    }
+
+    //public class
+
+    //[UsedImplicitly]
+    //[StaticConstructorOnStartup]
+    //public static class Setup
+    //{
+    //    static Setup()
+    //    {
+    //        var harmony = new Harmony(Content.PackageIdPlayerFacing);
+    //        harmony.PatchAll();
+    //    }
+    //}
+}
---
+++ 1.5/[1001]ArtificialSkipdoors/NVPEDoorBuilding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse.AI;
+using VFECore;
+using VanillaPsycastsExpanded;
+using VanillaPsycastsExpanded.Skipmaster;
+using VFECore.Abilities;
+
+
+
+using Prepatcher;
+
+namespace _1001_ArtificialSkipdoors
+{
+    public class NVPEDoorBuilding : Building
+    {
+        public Skipdoor pdoor;
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            pdoor = (Skipdoor)ThingMaker.MakeThing(VPE_DefOf.VPE_Skipdoor, null);
+            if(pdoor == null) { Log.Message("Sdoor null"); }
+            //Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(pdoor));
+            GenSpawn.Spawn(pdoor, Position + new IntVec3(0, 0, -1).RotatedBy(Rotation), map, WipeMode.Vanish);
+        }
+        public override void Tick()
+        {
+            base.Tick();
+        }
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            base.DeSpawn(mode);
+            pdoor.Destroy();
+        }
+    }
+
+
+}
