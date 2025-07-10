13ead45ad6e38c9bf4fb2aeb6ec6bcd03a2eae28
papercrane1001 <papercrane1001@gmail.com>
2025-07-10 14:58:02 -0700

    temp

---
+++ 1.6/[1001]ArtificialSkipdoors/HarmonyPatches.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+using VanillaPsycastsExpanded.Skipmaster;
+using System.Reflection.Emit;
+using VFECore;
+
+namespace _1001_ArtificialSkipdoors
+{
+    [HarmonyPatch(typeof(Skipdoor), "SpawnSetup")]
+    public static class SkipDoor_SpawnSetup_Transpiler
+    {
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(Skipdoor), "SpawnSetup");
+        }
+        public static IEnumerable<CodeInstruction> SkipDoor_SpawnSetup_Actual_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
+        {
+            //Log.Message("ASTranspiler Running");
+            List<CodeInstruction> instructionList = instructions.ToList();
+            bool found = false;
+
+            FieldInfo pawnInfo = AccessTools.Field(typeof(Skipdoor), nameof(Skipdoor.Pawn));
+            
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                if (instructionList[i].opcode == OpCodes.Ldarg_0 && i + 2 < instructionList.Count && found == false &&
+                    instructionList[i + 1].LoadsField(pawnInfo) &&
+                    instructionList[i + 2].opcode == OpCodes.Call)
+                {
+                    Log.Message("Found relevant method call, running patch");
+                    found = true;
+
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, pawnInfo);
+                    Label label = ilg.DefineLabel();
+
+                    yield return new CodeInstruction(OpCodes.Brtrue, label);
+                    yield return new CodeInstruction(OpCodes.Ret);
+                    yield return new CodeInstruction(OpCodes.Nop).WithLabels(label);
+
+                    yield return instructionList[i];
+                        
+                }
+                else
+                {
+                    yield return instructionList[i];
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch]
+    public static class HarmonyPatches
+    {
+        public static IEnumerable<Gizmo> GetJustRenameGizmo(Skipdoor door)
+        {
+            VFECore.DoorTeleporterExtension extension = door.def.GetModExtension<VFECore.DoorTeleporterExtension>();
+            DoorTeleporterMaterials doorMaterials = DoorTeleporter.doorTeleporterMaterials[door.def];
+
+            if (doorMaterials.RenameIcon != null)
+            {
+                yield return new Command_Action 
+                {
+                    defaultLabel = extension.renameLabelKey.Translate(),
+                    defaultDesc = extension.renameDescKey.Translate(),
+                    icon = doorMaterials.RenameIcon,
+                    action = delegate
+                    {
+                        //Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(door));
+                    }
+                };
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Skipdoor), "GetDoorTeleporterGismoz")]
+    public static class SkipDoor_GetDoorTeleporterGismoz_Transpiler
+    {
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(Skipdoor), "GetDoorTeleporterGismoz");
+        }
+
+
+        public static IEnumerable<CodeInstruction> SkipDoor_GetDoorTeleporterGismoz_Actual_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList();
+            bool found1 = false;
+            bool found2 = false;
+            FieldInfo pawnInfo = AccessTools.Field(typeof(Skipdoor), nameof(Skipdoor.Pawn));
+
+            Label label = ilg.DefineLabel();
+
+            for (int i = 0; i < instructionList.Count; i++){
+                if (
+                    //i == 1)
+                    found1 == false &&
+                    found2 == false &&
+                    instructionList[i].opcode == OpCodes.Newobj
+                    )
+
+                {
+                    found1 = true;
+                    MethodInfo myRenameGizmo = AccessTools.Method(
+                        typeof(HarmonyPatches), nameof(HarmonyPatches.GetJustRenameGizmo));
+                    
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, pawnInfo);                    
+
+                    yield return new CodeInstruction(OpCodes.Brfalse, label);
+
+                    yield return instructionList[i];
+
+
+                }
+                else if(
+                    found1==true &&
+                    found2 == false &&
+                    instructionList[i].opcode == OpCodes.Ldarg_0
+                    )
+                {
+                    found2 = true;
+                    yield return new CodeInstruction(OpCodes.Nop).WithLabels(label);
+                    yield return instructionList[i];
+                }
+                else
+                {
+                    yield return instructionList[i];
+                }
+                
+            }
+        }
+    }
+
+
+    /*
+     * public override IEnumerable<Gizmo> GetDoorTeleporterGismoz()
+		{
+			DoorTeleporterExtension extension = this.def.GetModExtension<DoorTeleporterExtension>();
+			DoorTeleporterMaterials doorMaterials = DoorTeleporter.doorTeleporterMaterials[this.def];
+			bool flag = doorMaterials.DestroyIcon != null;
+			if (flag)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = extension.destroyLabelKey.Translate(),
+					defaultDesc = extension.destroyDescKey.Translate(this.Pawn.NameFullColored),
+					icon = doorMaterials.DestroyIcon,
+					action = delegate()
+					{
+						this.Destroy(DestroyMode.Vanish);
+					}
+				};
+			}
+			bool flag2 = doorMaterials.RenameIcon != null;
+			if (flag2)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = extension.renameLabelKey.Translate(),
+					defaultDesc = extension.renameDescKey.Translate(),
+					icon = doorMaterials.RenameIcon,
+					action = delegate()
+					{
+						Find.WindowStack.Add(new Dialog_RenameDoorTeleporter(this));
+					}
+				};
+			}
+			yield break;
+		}
+    */
+}
---
+++ 1.6/[1001]ArtificialSkipdoors/ModEntry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime;
+using HarmonyLib;
+using JetBrains.Annotations;
+//using TerrainMovement;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace _1001_ArtificialSkipdoors
+{
+    [UsedImplicitly]
+    public class ModEntry : Mod
+    {
+        public ModEntry(ModContentPack content) : base(content)
+        {
+            var har = new Harmony(Content.PackageIdPlayerFacing);
+            //var vfeCoreAssembly = typeof(VFECore.DoorTeleporterExtension).Assembly;
+            var vfeCoreAssembly = typeof(VFECore.ShieldUtility).Assembly;
+            Log.Message($"[ArtificialSkipdoors] Loaded VFECore assembly: {vfeCoreAssembly.FullName}");
+            try
+            {
+                System.Reflection.MethodInfo mOriginal =
+                AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "SpawnSetup");
+                var mTranspiler = AccessTools.Method(
+                    typeof(SkipDoor_SpawnSetup_Transpiler),
+                    "SkipDoor_SpawnSetup_Actual_Transpiler");
+                if (har == null) { Log.Message("har null"); }
+                if (mOriginal == null) { Log.Message("mOriginal null"); }
+                if (mTranspiler == null) { Log.Message("mTranspiler null"); }
+                har.Patch(mOriginal, null, null, new HarmonyMethod(mTranspiler));
+
+                System.Reflection.MethodInfo mOGUI =
+                    AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "GetDoorTeleporterGismoz");
+                var mTrGUI = AccessTools.Method(
+                    typeof(SkipDoor_GetDoorTeleporterGismoz_Transpiler),
+                    "SkipDoor_GetDoorTeleporterGismoz_Actual_Transpiler");
+                har.Patch(mOGUI, null, null, new HarmonyMethod(mTrGUI));
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[ArtificialSkipdoors] Error patching methods: {e}");
+            }
+        }
+
+        
+    }
+}
