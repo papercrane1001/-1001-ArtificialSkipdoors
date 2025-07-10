909068ae904663b1eea50ea6495951ef42916284
papercrane1001 <papercrane1001@gmail.com>
2025-07-10 15:00:32 -0700

    Technically stable and functional, somehow

--- 1.6/[1001]ArtificialSkipdoors/ModEntry.cs
+++ 1.6/[1001]ArtificialSkipdoors/ModEntry.cs
@@ -20,32 +20,22 @@ namespace _1001_ArtificialSkipdoors
         public ModEntry(ModContentPack content) : base(content)
         {
             var har = new Harmony(Content.PackageIdPlayerFacing);
-            //var vfeCoreAssembly = typeof(VFECore.DoorTeleporterExtension).Assembly;
-            var vfeCoreAssembly = typeof(VFECore.ShieldUtility).Assembly;
-            Log.Message($"[ArtificialSkipdoors] Loaded VFECore assembly: {vfeCoreAssembly.FullName}");
-            try
-            {
-                System.Reflection.MethodInfo mOriginal =
+            System.Reflection.MethodInfo mOriginal = 
                 AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "SpawnSetup");
-                var mTranspiler = AccessTools.Method(
-                    typeof(SkipDoor_SpawnSetup_Transpiler),
-                    "SkipDoor_SpawnSetup_Actual_Transpiler");
-                if (har == null) { Log.Message("har null"); }
-                if (mOriginal == null) { Log.Message("mOriginal null"); }
-                if (mTranspiler == null) { Log.Message("mTranspiler null"); }
-                har.Patch(mOriginal, null, null, new HarmonyMethod(mTranspiler));
+            var mTranspiler = AccessTools.Method(
+                typeof(SkipDoor_SpawnSetup_Transpiler),
+                "SkipDoor_SpawnSetup_Actual_Transpiler");
+            if(har == null) { Log.Message("har null"); }
+            if(mOriginal == null) { Log.Message("mOriginal null"); }
+            if(mTranspiler == null) { Log.Message("mTranspiler null"); }
+            har.Patch(mOriginal, null, null, new HarmonyMethod(mTranspiler));
 
-                System.Reflection.MethodInfo mOGUI =
-                    AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "GetDoorTeleporterGismoz");
-                var mTrGUI = AccessTools.Method(
-                    typeof(SkipDoor_GetDoorTeleporterGismoz_Transpiler),
-                    "SkipDoor_GetDoorTeleporterGismoz_Actual_Transpiler");
-                har.Patch(mOGUI, null, null, new HarmonyMethod(mTrGUI));
-            }
-            catch (Exception e)
-            {
-                Log.Error($"[ArtificialSkipdoors] Error patching methods: {e}");
-            }
+            System.Reflection.MethodInfo mOGUI = 
+                AccessTools.Method(typeof(VanillaPsycastsExpanded.Skipmaster.Skipdoor), "GetDoorTeleporterGismoz");
+            var mTrGUI = AccessTools.Method(
+                typeof(SkipDoor_GetDoorTeleporterGismoz_Transpiler),
+                "SkipDoor_GetDoorTeleporterGismoz_Actual_Transpiler");
+            har.Patch(mOGUI, null, null, new HarmonyMethod(mTrGUI));
         }
 
         
